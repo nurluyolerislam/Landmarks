aa2fc5578065b1753aef3ea567cdafd8924b8d44
nurluyolerislam <nurluyolerislam@gmail.com>
2023-03-24 01:41:08 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Erislam Nurluyol on 24.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
