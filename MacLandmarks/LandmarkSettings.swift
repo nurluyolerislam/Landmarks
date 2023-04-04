8d404b83cdbce2b1b78849a6ea9b103a7ba11e9f
nurluyolerislam <nurluyolerislam@gmail.com>
2023-04-04 14:17:08 +0300

    Creating a macOS App

---
+++ MacLandmarks/LandmarkSettings.swift
@@ -0,0 +1,33 @@
+//
+//  LandmarkSettings.swift
+//  MacLandmarks
+//
+//  Created by Erislam Nurluyol on 4.04.2023.
+//
+
+import SwiftUI
+
+struct LandmarkSettings: View {
+    @AppStorage("MapView.zoom")
+    private var zoom: MapView.Zoom = .medium
+    
+    var body: some View {
+        Form {
+            Picker("Map Zoom:", selection: $zoom) {
+                ForEach(MapView.Zoom.allCases) { level in
+                    Text(level.rawValue)
+                }
+            }
+            .pickerStyle(.inline)
+        }
+        .frame(width: 300)
+        .navigationTitle("Landmark Settings")
+        .padding(80)
+    }
+}
+
+struct LandmarkSettings_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkSettings()
+    }
+}
